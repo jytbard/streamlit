7590154083bf323ac506ab1ec1c19770e8a39d86
jytbard <111190566+jytbard@users.noreply.github.com>
2024-01-15 22:48:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,32 +14,3 @@ df = conn.query("select * from WATCHTOWER_STATIONS_TBL;", ttl=600)
 
 print(df)
 
-# Print results.
-#for row in df.itertuples():
-   # st.write(f"{row.ID} has a :{row.ORGANIZATIONUNITID}:")
-
-#df.groupby('Team')
-
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
-
-
-
-
-#pja45566.east-us-2.azure
