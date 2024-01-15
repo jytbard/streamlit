bd27d6d748ab872db430f17da77eca0f82bed07d
jytbard <111190566+jytbard@users.noreply.github.com>
2024-01-15 23:11:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 import numpy as np
 
-import plotly.figure_factory as ff
+import plotly.express as px
 
 # Initialize connection.
 conn = st.connection("snowflake")
@@ -16,4 +16,18 @@ df = conn.query("select * from WATCHTOWER_STATIONS_TBL;", ttl=600)
 for row in df.itertuples():
    st.write(f"{row.ID} has a :{row.ORGANIZATIONUNITID}:")
 
+st.subheader("Define a custom colorscale")
 
+fig = px.scatter(
+    df,
+    x="ORGANIZATIONUNITID",
+    y="ID",
+    color="ID",
+    color_continuous_scale="reds",
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    st.plotly_chart(fig, theme=None, use_container_width=True)
