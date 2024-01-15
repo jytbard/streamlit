292a61280b25bfa9aa43f460a74360b663bcf280
jytbard <111190566+jytbard@users.noreply.github.com>
2024-01-15 22:29:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@
 
 import streamlit as st
 
+import numpy as np
+
+import plotly.figure_factory as ff
+
 # Initialize connection.
 conn = st.connection("snowflake")
 
@@ -9,8 +13,27 @@ conn = st.connection("snowflake")
 df = conn.query("select * from WATCHTOWER_STATIONS_TBL;", ttl=600)
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.ID} has a :{row.ORGANIZATIONUNITID}:")
+#for row in df.itertuples():
+   # st.write(f"{row.ID} has a :{row.ORGANIZATIONUNITID}:")
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+
 
 
 #pja45566.east-us-2.azure
